206f7ea9a0642e30738ee7a75ba1271d22b226ef
frank19900731 <scr_0731@126.com>
2014-10-27 16:31:20 +0800

    first commit

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 set -eu
 
-svn update
 application_home=$(pushd $(dirname $0) >/dev/null && pwd -P && popd >/dev/null)
 mvn clean package -U -DskipTests
 cp $application_home/target/*-jar-with-dependencies.jar $application_home/lib/
